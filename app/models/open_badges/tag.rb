c4e5b25922e85f2681ea69733c19c879cb392b57
Andrew Eng <andrew.engwy@gmail.com>
2013-02-26 18:50:01 +0800

    Added validation, dependency checks for Badge, BadgeTag and Tag

--- app/models/open_badges/tag.rb
+++ app/models/open_badges/tag.rb
@@ -1,5 +1,22 @@
 module OpenBadges
   class Tag < ActiveRecord::Base
+    has_many :badge_tags
+
+    before_destroy :ensure_not_referenced_by_any_badge_tag
+
     attr_accessible :name
+    
+    validates :name, uniqueness: true
+    validates :name, presence: true
+
+    def ensure_not_referenced_by_any_badge_tag
+      if badge_tags.empty?
+        return true
+      else
+        errors.add(:base, 'Tag is associated with at least 1 badge')
+        logger.debug('Tag is associated with at least 1 badge')
+        return false
+      end
+    end
   end
 end
