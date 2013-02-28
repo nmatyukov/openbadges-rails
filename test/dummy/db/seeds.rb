cb7ff29d11d78bbf1897ec81eeee85c8160216fb
Andrew Eng <andrew.engwy@gmail.com>
2013-02-28 14:51:13 +0800

    Update db seed

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,30 +1,61 @@
 OpenBadges::Badge.delete_all
 OpenBadges::Tag.delete_all
 OpenBadges::BadgeTag.delete_all
+OpenBadges::Alignment.delete_all
+OpenBadges::BadgeAlignment.delete_all
 
 OpenBadges::Badge.create(
-  name: 'Badge 1',
-  image: 'badge1.png')
+  id: 1,
+  name: 'Teleport',
+  image: 'teleport.png')
 OpenBadges::Badge.create(
-  name: 'Badge 2',
-  image: 'badge2.jpg')
+  id: 2,
+  name: 'Ice Wall',
+  image: 'ice_wall.jpg')
 OpenBadges::Badge.create(
-  name: 'Badge 3',
-  image: 'badge3.jpeg')
+  id: 3,
+  name: 'Fireball',
+  image: 'fireball.jpeg')
 
 OpenBadges::Tag.create(
-  name: 'Tag 1')
+  id: 1,
+  name: 'Magic')
 OpenBadges::Tag.create(
-  name: 'Tag 2')
-OpenBadges::Tag.create(
-  name: 'Tag 3')
+  id: 2,
+  name: 'Elemental')
 
 OpenBadges::BadgeTag.create(
   badge_id: 1,
   tag_id: 1)
 OpenBadges::BadgeTag.create(
   badge_id: 2,
+  tag_id: 1)
+OpenBadges::BadgeTag.create(
+  badge_id: 3,
+  tag_id: 1)
+OpenBadges::BadgeTag.create(
+  badge_id: 2,
   tag_id: 2)
 OpenBadges::BadgeTag.create(
   badge_id: 3,
-  tag_id: 3)
+  tag_id: 2)
+
+OpenBadges::Alignment.create(
+  id: 1,
+  name: 'Manipulated space time without time dilation',
+  url: 'www.hogwarts.edu/time_dilation')
+OpenBadges::Alignment.create(
+  id: 2,
+  name: 'Apply elemental knowledge to cast spells that are more useful',
+  url: 'www.hogwarts.edu/elemental_theory')
+
+OpenBadges::BadgeAlignment.create(
+  badge_id: 1,
+  alignment_id: 1)
+OpenBadges::BadgeAlignment.create(
+  badge_id: 2,
+  alignment_id: 2)
+OpenBadges::BadgeAlignment.create(
+  badge_id: 3,
+  alignment_id: 3)
+
