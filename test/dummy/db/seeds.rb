8c5745c6110de7d0d3f0da2dad1e34645c5ff0e5
Andrew Eng <andrew.engwy@gmail.com>
2013-02-28 19:18:44 +0800

    Added basic UI to badge index page

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -7,15 +7,18 @@ OpenBadges::BadgeAlignment.delete_all
 OpenBadges::Badge.create(
   id: 1,
   name: 'Teleport',
-  image: 'teleport.png')
+  image: 'teleport.png',
+  description: 'Manipulate space time to instantaneously travel from one location to another')
 OpenBadges::Badge.create(
   id: 2,
   name: 'Ice Wall',
-  image: 'ice_wall.jpg')
+  image: 'ice_wall.png',
+  description: 'Concentrate water vapour into a wall like structure and rapidly decrease its temperature')
 OpenBadges::Badge.create(
   id: 3,
   name: 'Fireball',
-  image: 'fireball.jpeg')
+  image: 'fireball.png',
+  description: 'Vibrate gas molecules violently within a confined space to create a ball of fire')
 
 OpenBadges::Tag.create(
   id: 1,
